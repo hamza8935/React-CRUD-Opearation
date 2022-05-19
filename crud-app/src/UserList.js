c709b52ca382a1e381704c50749cf9af8c692ce5
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-05-19 14:00:41 +0500

    update UI

--- crud-app/src/UserList.js
+++ crud-app/src/UserList.js
@@ -1,14 +1,75 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function UserList() {
-  return (
-    <div>
+    return (
+        <div>
 
-<div className=''>
+            <div className='container'>
+                <div className='row d-flex align-items-center mt-2 justify-content-around'>
+                    <div className='col-lg-6 col-md-6 col-sm-10 d-flex justify-content-around'>
+                        <p>User one</p>
 
-</div>
+
+                        <div>
+                            <button type="button" class="btn btn-danger me-3">Delete</button>
+                            <Link to='/edit/1' className="btn btn-primary">Edit</Link>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
 
 
-    </div>
-  )
+
+
+
+
+
+
+            <div className='container'>
+                <div className='row d-flex align-items-center mt-2 justify-content-around'>
+                    <div className='col-lg-6 col-md-6 col-sm-10 d-flex justify-content-around'>
+                        <p>User one</p>
+
+
+                        <div>
+                            <button type="button" class="btn btn-danger me-3">Delete</button>
+                            <Link to='/edit/1' className="btn btn-primary">Edit</Link>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            
+            <div className='container'>
+                <div className='row d-flex align-items-center mt-2 justify-content-around'>
+                    <div className='col-lg-6 col-md-6 col-sm-10 d-flex justify-content-around'>
+                        <p>User one</p>
+
+
+                        <div>
+                            <button type="button" class="btn btn-danger me-3">Delete</button>
+                            <Link to='/edit/1' className="btn btn-primary">Edit</Link>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+    )
 }
--- crud-app/src/components/Home.js
+++ crud-app/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import UserList from '../UserList'
 import Heading from './Heading'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
 
 
       <Heading />
+      <UserList />
 
 
 
