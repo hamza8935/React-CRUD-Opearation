9a349129add4ed3b2834ecace395f7306efa54cc
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-05-21 21:56:46 +0500

    Update AddUser.js

--- crud-app/src/components/AddUser.js
+++ crud-app/src/components/AddUser.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext , useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { GlobalContext } from '../context/GlobalState';
+import { v4 as uuidv4 } from 'uuid';
 
 
 export default function AddUser() {
+  const [name, setName] = useState( '' )
   const { addUser } = useContext(GlobalContext)
   const navigate = useNavigate();
 
   function submit() {
-    const newUser = {
-      id: 5,
-      name: 'Talha',
-    }
+    const newUser = {id: uuidv4(),name} 
+     
     navigate('/');
     addUser(newUser)
   }
+  function change( e ) {
+
+    setName(e.target.value)
   
+  }
 
 
 
@@ -29,7 +33,7 @@ return (
 
         <div className='col-lg-6 col-md-6 col-sm-10 mt-5 '>
           <label className='mb-2' htmlFor="abc">Enter your name</label>
-          <input type="text" id='abc' class="form-control shadow-none mb-2 " placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" />
+          <input type="text" id='abc' onChange={change} class="form-control shadow-none mb-2 " placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" />
           <button onClick={submit} type="button" class="btn btn-danger me-3">submit</button>
           <Link to='/' className="btn btn-primary">cancel</Link>
 
